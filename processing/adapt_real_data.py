454ede45d5a7c2b83b2927f8ca6abe1d3f9fb3a5
Juan Sandubete <jsandlop@gmail.com>
2024-08-30 18:14:02 +0200

    The script to process the experimental data is already operative

--- processing/adapt_real_data.py
+++ processing/adapt_real_data.py
@@ -3,6 +3,8 @@ import numpy as np
 import sys, os
 import matplotlib.pyplot as plt
 
+from scipy import interpolate
+
 
 class user_inputer:
     def __init__(self) -> None:
@@ -10,30 +12,38 @@ class user_inputer:
         self.filenames = []
         self.last_t_pair = []
         self.current_filename = ""
+        self.y_min = 0.0
+        self.y_max = 2000
+        self.lines_c = ["g", "r"]
         
     def onclick(self, event):
         """
-        Interactive plot from user @armatita on Stackoverflow: 
-        https://stackoverflow.com/questions/37363755/
-        python-mouse-click-coordinates-as-simply-as-possible
+        Event tracker for the plot. It is used to catch the actions from
+        the user s.t. the signal can be split in the right sequences.
         """
         if len(self.last_t_pair) < 2:
             self.last_t_pair.append(event.xdata)
+
+            plt.vlines(event.xdata, ymin=self.y_min, ymax=self.y_max, 
+                       colors=self.lines_c[len(self.last_t_pair)-1])
+            plt.draw()
         if len(self.last_t_pair) == 2:
-            self.t_pairs.append( self.last_t_pair )
+            self.t_pairs[-1].append( self.last_t_pair )
             self.filenames.append( self.current_filename )
             self.last_t_pair = []
         print( "t: {:.3f}s".format(event.xdata) )
 
 
 def adapt_data_format():
-    if len(sys.argv) != 2:
-        print("A folder containing the data must be specified.")
+    if len(sys.argv) != 3:
+        print("Data folder and out sampling points must be specified.")
         exit()
     datapath = sys.argv[1]
+    n_out_points = int(sys.argv[2])
     data_cols = ["Time [s]", "P1(Read)[mbar]", "P2(Read)[mbar]"]
     u_inputer = user_inputer()
     X, Y = [], []
+    incT = []
 
     # Gather all the files' names
     files = []
@@ -58,23 +68,55 @@ def adapt_data_format():
         filename = row["signal"]+ "_" + \
                    row["p_range"]+ "_" + \
                    "{}C.txt".format(row["avg_temp"])
+        # Update user_inputer values
         u_inputer.current_filename = filename
+        u_inputer.t_pairs.append( [] )
         sigs_df = pd.read_csv(datapath + "/" + filename, sep="\t")
-        print(sigs_df)
+        u_inputer.y_min = min(
+            sigs_df[data_cols[1]].min(),
+            sigs_df[data_cols[2]].min()
+        )
+        u_inputer.y_max = max(
+            sigs_df[data_cols[1]].max(),
+            sigs_df[data_cols[2]].max()
+        )
 
         # Interactive plot
         fig, ax = plt.subplots()
         plt.plot(sigs_df[data_cols[0]], sigs_df[data_cols[1]], label="P1")
         plt.plot(sigs_df[data_cols[0]], sigs_df[data_cols[2]], label="P2")
         plt.legend()
+        plt.grid(True)
         fig.canvas.mpl_connect('button_press_event', u_inputer.onclick)
         plt.show()
 
-        # Append the signals to a numpy array
-        # Append the label of the signal (viscosity)
+        # Get the data into the right format
+        t = sigs_df[data_cols[0]].tolist()
+        p1 = sigs_df[data_cols[1]].tolist()
+        p2 = sigs_df[data_cols[2]].tolist()
+        for t_pair in u_inputer.t_pairs[-1]:
+            # Get the index of closest t (for start and end)
+            idx_start = min(range(len(t)), key=lambda i: abs(t[i]-t_pair[0]))
+            idx_end = min(range(len(t)), key=lambda i: abs(t[i]-t_pair[1]))
+            # Interpolate data
+            f1 = interpolate.interp1d(t[idx_start:idx_end], p1[idx_start:idx_end])
+            f2 = interpolate.interp1d(t[idx_start:idx_end], p2[idx_start:idx_end])
+            itp_t = np.linspace(t[idx_start], t[idx_end-1], n_out_points)
+            # Append the signals to a numpy array as well as viscosity and period
+            X.append( np.transpose([f1(itp_t), f2(itp_t)]) )
+            Y.append( row["viscosity"] )
+            incT.append( itp_t[1]-itp_t[0] )
     
-    # Normalize the data
     # Save the data as numpy X and Y matrices
+    if not os.path.exists(datapath + "/processed"):
+        os.mkdir( datapath + "/processed" )
+
+    with open(datapath + "/processed/metadata.txt", "w") as f: 
+        f.write("Origin folder: {}\n".format(datapath))
+        f.write("N samples: {}\n".format(n_out_points))
+    np.save(datapath + "/processed/X_samples.npy", X)
+    np.save(datapath + "/processed/Y_samples.npy", Y)
+    np.save(datapath + "/processed/incT_samples.npy", incT)
 
 
 if __name__ == "__main__":
